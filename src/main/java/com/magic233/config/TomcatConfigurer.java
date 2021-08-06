b33f37b4a338b4e6a4d1f2b7c17bd3e78809ac23
邹渊 <1946407125@qq.com>
2021-08-06 03:18:28 +0000

    加入tomcat配置文件

---
+++ src/main/java/com/magic233/config/TomcatConfigurer.java
@@ -0,0 +1,27 @@
+package com.magic233.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: TomcatConfigurer
+ * Description:
+ * date: 2021/8/6 9:58
+ * author: zouyuan
+ */
+
+@Configuration
+public class TomcatConfigurer {
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+        });
+        return factory;
+    }
+}
